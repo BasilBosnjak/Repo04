9fff529593a60bef30171b729cbcd0eba2129c5f
BasilBosnjak <basil.bosnjak@gmail.com>
2021-09-17 00:58:34 +0200

    Adding exercises 04 to 09

---
+++ ex09/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+
+    var firstNum, secondNum, thirdNum;
+    firstNum = 6;
+    secondNum = 25;
+    thirdNum = 5.2;
+
+    firstNum *= 9;
+    thirdNum *= 10;
+    secondNum /= 5;
+
+    return {
+        firstNum,
+        secondNum,
+        thirdNum
+    };
+}
+
+console.log(main());
+module.exports = main;
