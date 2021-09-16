722a83203bee5bd7bf5d45bfe017237fc3250417
BasilBosnjak <basil.bosnjak@gmail.com>
2021-09-17 01:09:40 +0200

    Adding exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    var myFirstName, myLastName, myHomeTown;
+    myFirstName = "Basil";
+    myLastName = "Bosnjak";
+    myHomeTown = "Mostar";
+
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
